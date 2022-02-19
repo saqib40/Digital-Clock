87420bd532022c0113109cb0bf8d0d64d037663f
saqib hussain <hussainsakib44@gmail.com>
2022-02-19 07:26:12 +0530

    Create project.js

---
+++ project.js
@@ -0,0 +1,26 @@
+let heading = document.querySelector('h1');
+
+function showTime() {
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  
+  let AmPm = 'AM';
+  if (hour > 12) {
+    hour -= 12;
+    AmPm = 'PM';
+  }
+  if (hour == 0) {
+    hour = 12;
+    AmPm = 'AM';
+  }
+  
+  hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+  
+  heading.textContent = hour + ':' + min + ':' + sec + AmPm;
+}
+
+setInterval(showTime, 1000);
